54b7058445d71d3d5a33e2045459a105cb765fb9
aura2062 <aura_2062@proton.me>
2026-07-05 09:33:47 +0200

    start.string.io setup completed

---
+++ src/main/java/com/example/mhlmbackend/MhlmbackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.mhlmbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MhlmbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MhlmbackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mhlmbackend/MhlmbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mhlmbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MhlmbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
