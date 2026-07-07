6275d3f74be12260ebe9727a2fbd95ca40cea55b
aura2062 <aura_2062@proton.me>
2026-07-07 15:48:22 +0200

    forgot an import in WebConfig lmao, clumsy me

--- src/main/java/com/example/mhlmbackend/config/WebConfig.java
+++ src/main/java/com/example/mhlmbackend/config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.example.mhlmbackend.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
