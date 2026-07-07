f95b6c7cc4758b1630e321f0f30358c832fbff89
aura2062 <aura_2062@proton.me>
2026-07-07 14:18:57 +0200

    rewritten ArmorLoadoutRepoistoryTest and fully implemented all CRUD tests of ArmorLoadoutServiceTest, ready for starting 06A now

--- src/test/java/com/example/mhlmbackend/repository/ArmorLoadoutRepositoryTest.java
+++ src/test/java/com/example/mhlmbackend/repository/ArmorLoadoutRepositoryTest.java
@@ -1,13 +1,15 @@
 package com.example.mhlmbackend.repository;
 
 import com.example.mhlmbackend.model.ArmorLoadout;
+import com.example.mhlmbackend.model.ArmorPiece;
+import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 public class ArmorLoadoutRepositoryTest {
@@ -15,10 +17,45 @@ public class ArmorLoadoutRepositoryTest {
     @Autowired
     private ArmorLoadoutRepository armorLoadoutRepository;
 
-    @Test
-    void seederHasDbFilledWithAtLeastOneArmorLoadout() {
-        long count =  armorLoadoutRepository.count();
+    @Autowired
+    private ArmorPieceRepository armorPieceRepository;
 
-        assertTrue(count > 0);
+    @Test
+    @Transactional
+    void seederHasDbFilledWithAtLeastOneValidArmorLoadout() {
+        assertTrue(armorPieceRepository.findAll().size()>5);
+        String name = "test-loadout";
+        ArmorPiece head = armorPieceRepository.findById("1").orElse(null);
+        ArmorPiece chest = armorPieceRepository.findById("2").orElse(null);
+        ArmorPiece gloves = armorPieceRepository.findById("3").orElse(null);
+        ArmorPiece waist = armorPieceRepository.findById("4").orElse(null);
+        ArmorPiece legs = armorPieceRepository.findById("5").orElse(null);
+
+        assertNotNull(head);
+        assertNotNull(chest);
+        assertNotNull(gloves);
+        assertNotNull(waist);
+        assertNotNull(legs);
+
+        ArmorLoadout armorLoadout = new ArmorLoadout( name, head, chest, gloves, waist, legs );
+
+        armorLoadoutRepository.save(armorLoadout);
+
+        List<ArmorLoadout> listOfSavedLoadouts = armorLoadoutRepository.findAll();
+
+        ArmorLoadout foundCorrectArmorLoadoutInRepository = new ArmorLoadout();
+
+        for (ArmorLoadout loadout : listOfSavedLoadouts) {
+            if (loadout.getName().equals(name)) {
+                foundCorrectArmorLoadoutInRepository = loadout;
+            }
+        }
+
+        assertEquals(foundCorrectArmorLoadoutInRepository.getHeadArmorPiece().getName(), head.getName());
+        assertEquals(foundCorrectArmorLoadoutInRepository.getChestArmorPiece().getName(), chest.getName());
+        assertEquals(foundCorrectArmorLoadoutInRepository.getGlovesArmorPiece().getName(), gloves.getName());
+        assertEquals(foundCorrectArmorLoadoutInRepository.getWaistArmorPiece().getName(), waist.getName());
+        assertEquals(foundCorrectArmorLoadoutInRepository.getLegsArmorPiece().getName(), legs.getName());
+        assertTrue(foundCorrectArmorLoadoutInRepository.getId() >= 0);
     }
 }
--- src/test/java/com/example/mhlmbackend/service/ArmorLoadoutServiceTest.java
+++ src/test/java/com/example/mhlmbackend/service/ArmorLoadoutServiceTest.java
@@ -4,6 +4,7 @@ import com.example.mhlmbackend.dto.ArmorLoadoutFormDTO;
 import com.example.mhlmbackend.model.ArmorLoadout;
 import com.example.mhlmbackend.model.ArmorPiece;
 import com.example.mhlmbackend.repository.ArmorLoadoutRepository;
+import com.example.mhlmbackend.repository.ArmorPieceRepository;
 import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,46 +12,110 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
-public class ArmorLoadoutServiceTest { // rewrite this test file
+public class ArmorLoadoutServiceTest {
 
     @Autowired
     private ArmorLoadoutService armorLoadoutService;
 
     @Autowired
-    private ArmorLoadoutRepository armorLoadoutRepository;
+    private ArmorPieceRepository armorPieceRepository;
 
     @Test
-    void getAllArmorLoadoutsReturnsData() {
-        List<ArmorLoadout> armorLoadouts = armorLoadoutService.getAllArmorLoadouts();
+    @Transactional
+    void getAllArmorLoadoutsReturnsCorrectData() {
+        assertTrue(armorPieceRepository.findAll().size()>5);
+        String name = "test-loadout v2";
+        ArmorPiece head = armorPieceRepository.findById("1").orElse(null);
+        ArmorPiece chest = armorPieceRepository.findById("2").orElse(null);
+        ArmorPiece gloves = armorPieceRepository.findById("3").orElse(null);
+        ArmorPiece waist = armorPieceRepository.findById("4").orElse(null);
+        ArmorPiece legs = armorPieceRepository.findById("5").orElse(null);
 
-        assertFalse(armorLoadouts.isEmpty());
+        assertNotNull(head);
+        assertNotNull(chest);
+        assertNotNull(gloves);
+        assertNotNull(waist);
+        assertNotNull(legs);
+
+        ArmorLoadoutFormDTO form = new ArmorLoadoutFormDTO(name, head, chest, gloves, waist, legs);
+        ArmorLoadout toBeCreatedLoadout = armorLoadoutService.createArmorLoadout(form);
+
+        List<ArmorLoadout> listOfSavedLoadouts = armorLoadoutService.getAllArmorLoadouts();
+
+        ArmorLoadout foundCorrectArmorLoadout= new ArmorLoadout();
+
+        for (ArmorLoadout loadout : listOfSavedLoadouts) {
+            if (loadout.getId().equals(toBeCreatedLoadout.getId())) {
+                foundCorrectArmorLoadout = loadout;
+            }
+        }
+
+        assertEquals(foundCorrectArmorLoadout.getHeadArmorPiece().getName(), head.getName());
+        assertEquals(foundCorrectArmorLoadout.getChestArmorPiece().getName(), chest.getName());
+        assertEquals(foundCorrectArmorLoadout.getGlovesArmorPiece().getName(), gloves.getName());
+        assertEquals(foundCorrectArmorLoadout.getWaistArmorPiece().getName(), waist.getName());
+        assertEquals(foundCorrectArmorLoadout.getLegsArmorPiece().getName(), legs.getName());
+        assertTrue(foundCorrectArmorLoadout.getId() >= 0);
     }
 
     @Test
-    void getArmorLoadoutByIdReturnsCorrectArmorLoadout() {
-        ArmorLoadout armorLoadout = armorLoadoutService.getArmorLoadoutById("1");
-        assertNotNull(armorLoadout);
-        assertEquals("1", String.valueOf(armorLoadout.getId()));
+    @Transactional
+    void getArmorLoadoutByIdReturnsCorrectData() {
+        assertTrue(armorPieceRepository.findAll().size()>5);
+        String name = "test-loadout v3";
+        ArmorPiece head = armorPieceRepository.findById("1").orElse(null);
+        ArmorPiece chest = armorPieceRepository.findById("2").orElse(null);
+        ArmorPiece gloves = armorPieceRepository.findById("3").orElse(null);
+        ArmorPiece waist = armorPieceRepository.findById("4").orElse(null);
+        ArmorPiece legs = armorPieceRepository.findById("5").orElse(null);
+
+        assertNotNull(head);
+        assertNotNull(chest);
+        assertNotNull(gloves);
+        assertNotNull(waist);
+        assertNotNull(legs);
+
+        ArmorLoadoutFormDTO form = new ArmorLoadoutFormDTO(name, head, chest, gloves, waist, legs);
+        ArmorLoadout toBeCreatedLoadout = armorLoadoutService.createArmorLoadout(form);
+
+        List<ArmorLoadout> listOfSavedLoadouts = armorLoadoutService.getAllArmorLoadouts();
+
+        ArmorLoadout foundCorrectArmorLoadout= new ArmorLoadout();
+
+        for (ArmorLoadout loadout : listOfSavedLoadouts) {
+            if (loadout.getId().equals(toBeCreatedLoadout.getId())) {
+                foundCorrectArmorLoadout = loadout;
+            }
+        }
+
+        long correctId =  foundCorrectArmorLoadout.getId();
+
+        ArmorLoadout finalArmorLoadout = armorLoadoutService.getArmorLoadoutById(String.valueOf(correctId));
+
+        assertEquals(finalArmorLoadout.getHeadArmorPiece().getName(), head.getName());
+        assertEquals(finalArmorLoadout.getChestArmorPiece().getName(), chest.getName());
+        assertEquals(finalArmorLoadout.getGlovesArmorPiece().getName(), gloves.getName());
+        assertEquals(finalArmorLoadout.getWaistArmorPiece().getName(), waist.getName());
+        assertEquals(finalArmorLoadout.getLegsArmorPiece().getName(), legs.getName());
+        assertEquals((long) finalArmorLoadout.getId(), correctId);
     }
 
     @Test
     @Transactional
     void createArmorLoadoutPersistsToDb() {
-        // head, chest, gloves, waist, legs
-        ArmorPiece head = new ArmorPiece();
-        ArmorPiece chest = new ArmorPiece();
-        ArmorPiece gloves = new ArmorPiece();
-        ArmorPiece waist = new ArmorPiece();
-        ArmorPiece legs = new ArmorPiece();
+        assertTrue(armorPieceRepository.findAll().size()>5);
+        String name = "test-loadout v4";
+        ArmorPiece head = armorPieceRepository.findById("1").orElse(null);
+        ArmorPiece chest = armorPieceRepository.findById("2").orElse(null);
+        ArmorPiece gloves = armorPieceRepository.findById("3").orElse(null);
+        ArmorPiece waist = armorPieceRepository.findById("4").orElse(null);
+        ArmorPiece legs = armorPieceRepository.findById("5").orElse(null);
 
         ArmorLoadoutFormDTO form = new ArmorLoadoutFormDTO(
-                "test",
+                name,
                 head,
                 chest,
                 gloves,
@@ -58,10 +123,131 @@ public class ArmorLoadoutServiceTest { // rewrite this test file
                 legs
         );
 
-        ArmorLoadout saved =  armorLoadoutService.createArmorLoadout(form);
+        ArmorLoadout saved = armorLoadoutService.createArmorLoadout(form);
 
-        assertNotNull(saved.getId());
-        assertTrue(armorLoadoutRepository.findById(saved.getId()).isPresent());
-        assertEquals("test", saved.getName());
+        List<ArmorLoadout> listOfSavedLoadouts = armorLoadoutService.getAllArmorLoadouts();
+
+        ArmorLoadout foundCorrectArmorLoadout= new ArmorLoadout();
+        for (ArmorLoadout loadout : listOfSavedLoadouts) {
+            if (loadout.getId().equals(saved.getId())) {
+                foundCorrectArmorLoadout = loadout;
+            }
+        }
+
+        assertNotNull(foundCorrectArmorLoadout.getId());
+        assertEquals(name, foundCorrectArmorLoadout.getName());
+        assertEquals(foundCorrectArmorLoadout.getHeadArmorPiece().getName(), head.getName());
+        assertEquals(foundCorrectArmorLoadout.getChestArmorPiece().getName(), chest.getName());
+        assertEquals(foundCorrectArmorLoadout.getGlovesArmorPiece().getName(), gloves.getName());
+        assertEquals(foundCorrectArmorLoadout.getWaistArmorPiece().getName(), waist.getName());
+        assertEquals(foundCorrectArmorLoadout.getLegsArmorPiece().getName(), legs.getName());
+    }
+
+    @Test
+    @Transactional
+    void updateArmorLoadoutUpdatesCorrectly() {
+        assertTrue(armorPieceRepository.findAll().size()>5);
+        String name = "test-loadout v5";
+        ArmorPiece head = armorPieceRepository.findById("1").orElse(null);
+        ArmorPiece chest = armorPieceRepository.findById("2").orElse(null);
+        ArmorPiece gloves = armorPieceRepository.findById("3").orElse(null);
+        ArmorPiece waist = armorPieceRepository.findById("4").orElse(null);
+        ArmorPiece legs = armorPieceRepository.findById("5").orElse(null);
+
+        assertNotNull(head);
+        assertNotNull(chest);
+        assertNotNull(gloves);
+        assertNotNull(waist);
+        assertNotNull(legs);
+
+        ArmorLoadoutFormDTO form = new ArmorLoadoutFormDTO(name, head, chest, gloves, waist, legs);
+        ArmorLoadout toBeCreatedLoadout = armorLoadoutService.createArmorLoadout(form);
+
+        List<ArmorLoadout> listOfSavedLoadouts = armorLoadoutService.getAllArmorLoadouts();
+
+        ArmorLoadout foundCorrectArmorLoadout= new ArmorLoadout();
+
+        for (ArmorLoadout loadout : listOfSavedLoadouts) {
+            if (loadout.getId().equals(toBeCreatedLoadout.getId())) {
+                foundCorrectArmorLoadout = loadout;
+            }
+        }
+
+        long correctId =  foundCorrectArmorLoadout.getId();
+
+        String name2 = "test-loadout v6";
+        ArmorPiece head2 = armorPieceRepository.findById("6").orElse(null);
+        ArmorPiece chest2 = armorPieceRepository.findById("7").orElse(null);
+        ArmorPiece gloves2 = armorPieceRepository.findById("8").orElse(null);
+        ArmorPiece waist2 = armorPieceRepository.findById("9").orElse(null);
+        ArmorPiece legs2 = armorPieceRepository.findById("10").orElse(null);
+
+        ArmorLoadoutFormDTO updatedForm = new ArmorLoadoutFormDTO(name2, head2, chest2, gloves2, waist2, legs2);
+        armorLoadoutService.updateArmorLoadout(String.valueOf(correctId), updatedForm);
+
+        List<ArmorLoadout> listWithUpdatedLoadout = armorLoadoutService.getAllArmorLoadouts();
+
+        ArmorLoadout foundUpdatedLoadout = null;
+
+        for (ArmorLoadout loadout : listWithUpdatedLoadout) {
+            if (loadout.getId().equals(toBeCreatedLoadout.getId())) {
+                foundUpdatedLoadout = loadout;
+            }
+        }
+
+        assertNotNull(foundUpdatedLoadout);
+        assertEquals(name2, foundUpdatedLoadout.getName());
+        assertEquals(foundUpdatedLoadout.getHeadArmorPiece().getName(), head2.getName());
+        assertEquals(foundUpdatedLoadout.getChestArmorPiece().getName(), chest2.getName());
+        assertEquals(foundUpdatedLoadout.getGlovesArmorPiece().getName(), gloves2.getName());
+        assertEquals(foundUpdatedLoadout.getWaistArmorPiece().getName(), waist2.getName());
+        assertEquals(foundUpdatedLoadout.getLegsArmorPiece().getName(), legs2.getName());
+        assertEquals((long) foundUpdatedLoadout.getId(), correctId);
+    }
+
+    @Test
+    @Transactional
+    void deleteArmorLoadoutReturnsDeletesCorrectly() {
+        assertTrue(armorPieceRepository.findAll().size()>5);
+        String name = "test-loadout v7";
+        ArmorPiece head = armorPieceRepository.findById("1").orElse(null);
+        ArmorPiece chest = armorPieceRepository.findById("2").orElse(null);
+        ArmorPiece gloves = armorPieceRepository.findById("3").orElse(null);
+        ArmorPiece waist = armorPieceRepository.findById("4").orElse(null);
+        ArmorPiece legs = armorPieceRepository.findById("5").orElse(null);
+
+        ArmorLoadoutFormDTO form = new ArmorLoadoutFormDTO(
+                name,
+                head,
+                chest,
+                gloves,
+                waist,
+                legs
+        );
+
+        ArmorLoadout saved = armorLoadoutService.createArmorLoadout(form);
+
+        List<ArmorLoadout> listOfSavedLoadouts = armorLoadoutService.getAllArmorLoadouts();
+
+        ArmorLoadout foundCorrectArmorLoadout= new ArmorLoadout();
+        for (ArmorLoadout loadout : listOfSavedLoadouts) {
+            if (loadout.getId().equals(saved.getId())) {
+                foundCorrectArmorLoadout = loadout;
+            }
+        }
+
+        armorLoadoutService.deleteArmorLoadout(String.valueOf(foundCorrectArmorLoadout.getId()));
+
+        List<ArmorLoadout> listOfAllLoadouts = armorLoadoutService.getAllArmorLoadouts();
+
+        ArmorLoadout doesItStillExist = null;
+
+        for (ArmorLoadout loadout : listOfAllLoadouts) {
+            if (loadout.getId().equals(saved.getId())) {
+                doesItStillExist = loadout;
+            }
+        }
+
+        assertNull(doesItStillExist);
     }
 }
